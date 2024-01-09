871a2cbd0b80f03bfdb5507b379517c65182ee6d
SimonVoss <60580744+SimonVoss@users.noreply.github.com>
2024-01-09 11:05:44 +0100

    This is the main code for the whole project

---
+++ ProjectCode.ino
@@ -0,0 +1,39 @@
+#include <Bonezegei_DHT11.h>
+
+const int TempHum_Pin = 2;
+
+int previousMillis = 0;
+float Celcius, Humidity = 0;
+
+//param = DHT11 signal pin
+Bonezegei_DHT11 dht(TempHum_Pin);
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TempHum_Pin, INPUT);
+
+  dht.begin();
+}
+
+void loop() {
+
+  TempHumModule();
+}
+
+void TempHumModule() {
+  unsigned long currentMillis = millis();
+
+  // Check if the specified interval has elapsed
+  if (currentMillis - previousMillis >= interval) {
+    if (dht.getData()) {               // get All data from DHT11
+      Celcius = dht.getTemperature();  // return temperature in celsius
+      Humidity = dht.getHumidity();    // return humidity
+
+      //float tempFar = dht.getTemperature(true);  // return temperature in fahrenheit if true celsius if false
+    }
+    // Update the previousMillis for the next iteration
+    previousMillis = currentMillis;
+  }
+}
