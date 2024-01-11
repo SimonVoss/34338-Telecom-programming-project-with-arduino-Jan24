b77609c34aab38b4500b54a282503cd3f1811616
SimonVoss <60580744+SimonVoss@users.noreply.github.com>
2024-01-11 15:13:04 +0100

    Update ESP8266.ino

--- Fan/ESP8266/ESP8266.ino
+++ Fan/ESP8266/ESP8266.ino
@@ -1,20 +1,20 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial espSerial(3, 1);  // RX, TX
-int fanSpeed = 100;              // Example fan speed value
+int Fan_Speed = 100;              // Example fan speed value
 void setup() {
   // Start the ESP8266 serial communication at 9600 baud rate
   espSerial.begin(115200);
 }
 
 void loop() {
-
-  espSerial.println(fanSpeed);  // Send the fan speed to Arduino
+  Fan_Speed = 100;
+  espSerial.println(Fan_Speed);  // Send the fan speed to Arduino
   delay(2000);                  // Wait for a second
 
 
-  fanSpeed = 50;
-  espSerial.println(fanSpeed);
+  Fan_Speed = 50;
+  espSerial.println(Fan_Speed);
   delay(2000);  // Wait for a second
 
 }
